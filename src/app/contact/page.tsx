3acc4a225a0d7a7126844a3c4e1d60d2c105a0c7
María Candelaria Fariña Rodríguez <mfarinardguez@gmail.com>
2025-06-07 23:48:58 +0100

    Contact created

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,17 @@
+import ContactCard from "../components/ContactCard";
+import ContactForm from "../components/ContactForm";
+
+export default function ContactPage() {
+  return (
+    <section className="min-h-screen flex flex-col items-center justify-center px-4 py-10 bg-[#fef5f8]">
+      <div className="flex flex-col lg:flex-row gap-10 w-full max-w-6xl">
+        <div className="flex-1">
+          <ContactCard />
+        </div>
+        <div className="flex-1">
+          <ContactForm />
+        </div>
+      </div>
+    </section>
+  );
+}
